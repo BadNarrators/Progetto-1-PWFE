ee5b4c6a3cf4d0758fc421e6e83760ea508d728f
Andrea Atzei <andreaatzei01@gmail.com>
2021-04-04 13:12:59 +0200

    Updated index.html, sidebar.js and style.css for sidebar testing

--- script/sidebar.js
+++ script/sidebar.js
@@ -1,9 +1,5 @@
 function sidebar_push() {
-    if(document.getElementById("sidebar-content").style.display == "none"){
-        document.getElementById("sidebar-content").style.width = "100%";
-        document.getElementById("sidebar-content").style.display = "block";
-    }else{
-        document.getElementById("sidebar-content").style.display = "none";
-    }
+    sidebar.classList.toggle('is-closed');
 }
+
 
