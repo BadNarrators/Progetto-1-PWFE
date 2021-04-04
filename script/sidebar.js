1077a1160e195c2363d7d0b052992477e026e752
Andrea Atzei <andreaatzei01@gmail.com>
2021-04-04 11:35:51 +0200

    sidebar.js added and style.css modified

---
+++ script/sidebar.js
@@ -0,0 +1,8 @@
+function sidebar_open() {
+    document.getElementById("mySidebar").style.width = "100%";
+    document.getElementById("navbar").style.display = "block";
+}
+  
+function sidebar_close() {
+    document.getElementById("navbar").style.display = "none";
+}
