d1c6fd89e9ebf78de6aecf75848cc0e8c196fd31
Andrea Atzei <andreaatzei01@gmail.com>
2021-04-05 18:43:53 +0200

    Updated the sidebar

--- script/sidebar.js
+++ script/sidebar.js
@@ -1,5 +1,17 @@
-function sidebar_push() {
+function sidebarPush() {
     sidebar.classList.toggle('is-closed');
 }
 
+function Change(x) {
+  x.classList.toggle('change');
+}
 
+// Da fixare
+function sidebarToggle(x) {
+  Change(x);
+  let sidenav_button = document.getElementById('menu');
+  let main = document.getElementById('main');
+  let sidenav = document.getElementById('sidebar-content');
+  sidenav.classList.toggle('change');
+  sidebarPush();
+}
