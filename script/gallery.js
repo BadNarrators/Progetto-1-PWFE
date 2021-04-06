597e8a1b9b465f559ade318d3733a909702ff7a8
Alessandro Carabetta <carabetta.ale@gmail.com>
2021-04-06 16:50:21 +0200

    add comment on gallery.js

--- script/gallery.js
+++ script/gallery.js
@@ -15,12 +15,16 @@ function showSlides(n) {
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
+
+  // display "none" on on all slide img (only one slide img have display "block")
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
+  // remove "active" class on all dot (only one dot have "active" class)
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
+
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
