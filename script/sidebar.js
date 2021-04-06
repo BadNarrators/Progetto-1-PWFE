a9c6cecb9e6f54d0f231ae9d28a3174b5683f3e1
Andrea Atzei <andreaatzei01@gmail.com>
2021-04-06 09:15:43 +0200

    Fixes and removed excess code

--- script/sidebar.js
+++ script/sidebar.js
@@ -1,17 +1,10 @@
-function sidebarPush() {
-    sidebar.classList.toggle('is-closed');
-}
-
-function Change(x) {
+function sidebarPush(x) {
   x.classList.toggle('change');
 }
 
-// FIXME: ordinare e ripulire
-function sidebarToggle(x) {
-  Change(x);
-  let sidenav_button = document.getElementById('menu');
-  let main = document.getElementById('main');
-  let sidenav = document.getElementById('sidebar-content');
-  sidenav.classList.toggle('change');
-  sidebarPush();
+function sidebarToggle() {
+  let sidenav_button = document.getElementById('sidebar-button');
+  let sidenav = document.getElementById('sidebar');
+  sidebarPush(sidenav_button);
+  sidebarPush(sidenav);
 }
