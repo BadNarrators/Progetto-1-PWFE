1947ef965b237e02d6c94f9317d17289450fdabd
EugenioDiPaola <68637309+EugenioDiPaola@users.noreply.github.com>
2021-04-28 17:51:26 +0200

    .

--- public/script/main.js
+++ public/script/main.js
@@ -1,20 +1,37 @@
+/*
+ * classList is a list of the CSS classes associated with x
+ * toggle('y') adds y to the classes of classList if y does not already belong to classList, removes it if it's already present.
+ */
 function Change(x) {
-  x.classList.toggle('change');
+  x.classList.toggle("change");
 }
 
+/*
+ * openNav opens the sidebar.
+ */
 function openNav() {
-  let sidebar = document.getElementById('sidebar-content');
-  sidebar.style.width = '100%';
-  sidebar.style.opacity = '1';
+  let sidebar = document.getElementById("sidebar-content");
+  sidebar.style.width = "100%";
+  sidebar.style.opacity = "1";
 }
 
+/*
+ * openNav closes the sidebar.
+ */
 function closeNav() {
-  let sidebar = document.getElementById('sidebar-content');
-  sidebar.style.width = '0';
-  sidebar.style.opacity = '0';
+  let sidebar = document.getElementById("sidebar-content");
+  sidebar.style.width = "0";
+  sidebar.style.opacity = "0";
 }
 
+/*
+ * By default the boolean open is set to false (by default the sidebar is closed when the page is first loaded).
+ */
 let opened = false;
+
+/*
+ * open and close sidebar.
+ */
 function toggleNav(x) {
   Change(x);
   if (!opened) {
